500e8757f870d888c08c371f80446d6ba04efb6f
techtraaz <techtraazsl@gmail.com>
2025-05-11 13:17:49 +0530

    added naive string matching

---
+++ stringmatch.py
@@ -0,0 +1,12 @@
+def naive_string_matcher(T, P):
+    n = len(T)
+    m = len(P)
+    # start from 0 index 
+    for s in range(n - m + 1):
+        if T[s:s + m] == P:
+            print(f"Pattern occurs with shift {s}")
+
+
+T = "abcxabcdabcdabcy"
+P = "abc"
+naive_string_matcher(T, P)
